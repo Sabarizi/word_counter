e5a712d149f3e9b0bdfabff09decff7db8e6f4ea
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-04-17 00:14:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count words: "
+    }
+]);
+//white space removed
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+//print the word count the sentence to the console
+console.log(`your sentence word count is ${words.length}`);
